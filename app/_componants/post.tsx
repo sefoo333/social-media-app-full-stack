864347fd4f73705c1f7302082c1f74debdbb57ae
sefoo333 <111438242+sefoo333@users.noreply.github.com>
2024-09-06 22:24:24 +0300

    Update post.tsx

--- app/_componants/post.tsx
+++ app/_componants/post.tsx
@@ -211,7 +211,7 @@ function Post(props: any) {
                     </div>
 
                     <div className="content p-[10px] leading-[2]">
-   <Link href={`/post/${a.id}`}>
+   <Link href={`/post/${props.id}`}>
                         <p>{props.postname}</p>
        </Link>
                     </div>
